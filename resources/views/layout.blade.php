7199d4c03ebe66698a3f483319639e21ff760150
Exanlv <exan@landviz.nl>
2019-06-20 21:49:16 +0200

    Somewhat of a base thing set up

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+	<style>
+	html {
+		font-family: 'Nunito', sans-serif !important;
+	}
+
+	.main h1 {
+		color: #212529;
+	}
+
+	.main {
+		font-size: 20px;
+		color: #909090;
+	}
+	</style>
 </head>
 <body>
-	<h1>Hello world!</h1>
-	@yield('body')
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container container-fluid">
+			<h1>Inventory Management</h1>
+			<p>Easily manage inventory, maybe? If I get this to work I guess..</P>
+		</div>
+	</div>
+	<div class="container main">
+		@yield('body')
+	</div>
 </body>
 </html>
